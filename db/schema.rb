ca94debe4983f1cc27890a5888ba07c071e5e0f6
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2016-06-24 10:26:24 -0400

    making the app responsive

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160619173424) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bloodtypes", force: :cascade do |t|
     t.string   "group"
     t.datetime "created_at", null: false
