c1389ac839232c558ea4127376f728c793551dc6
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2016-06-26 19:50:02 -0400

    styled the flash messages

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,8 +11,23 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 
-	$(".notice").fadeOut(3000);
-	$(".alert").fadeOut(3000);
+	setTimeout(function(){
+		$("#notice-wrapper").fadeOut('slow', function(){
+			$(this).remove();
+		});
+	}, 4500);
+
+	// $(".notice").fadeOut(3000);
+	// $(".alert").fadeOut(3000);
 
 		// $(this).remove();
 });
+
+// or you can use this two lines of doing the same work. 
+
+// $(document).ready(function(){
+
+// 	// $(".notice").fadeOut(3000);
+// 	// $(".alert").fadeOut(3000);
+
+// });
--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,7 +2,7 @@ class Donor < ActiveRecord::Base
 	has_many :donations
 	has_many :patients, through: :donations
 	has_one :bloodtype
-	validates :name, :email, uniqueness: true
+	validates :name, :email, uniqueness: true, uniqueness: { case_sensitive: false }
 
 	def self.search(search)
 		if search
